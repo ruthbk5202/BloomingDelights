e6b4545d48102ddb86a0a8b71eee45ddaea9080d
Ruth Bishwakarma <ruthbishwokarma194@gmail.com>
2025-05-31 23:42:08 +0545

    style: update delivery info and styles Improved layout and spacing for delivery info form and promotion sections adjusted input and promotion code field styling for consistency

--- src/app/DeliveryInfoForm/page.tsx
+++ src/app/DeliveryInfoForm/page.tsx
@@ -4,6 +4,7 @@ import "./delivery.css";
 const DeliveryInfoForm = () => {
   return (
     <div className='delivery-info-container'>
+      <div>
             <h2 className='delivery-info-heading'>Delivery Information</h2>
         <form className='delivery-info-form'>
             <div>
@@ -49,6 +50,33 @@ const DeliveryInfoForm = () => {
             save</button>
             </div>
         </form>
+        </div>
+        <div className='delivery-grid-promotion'>
+          <form className='delivery-promotion-form'>
+            <div className='promotion-code'>
+              <input className="promotion-search" placeholder='Enter Store/blooming code'></input>
+              <button className='apply-btns'>Apply</button>
+
+            </div>
+            <div className='invoice-contact'>
+              <h3>Invoice and Contact Info</h3>
+              <a  href="/">Edit</a>
+            </div>
+            <div className='order-summary'>
+              <h3>Order Summary</h3>
+              <div className='order-summary-items'>
+              <p>items Total(1 items)</p> <h3>Rs. 299</h3>
+              </div>
+              <div className='order-summary-items'>
+              <p>Delivery Fees</p> <h3>Rs.100</h3>
+              </div>
+            </div>
+            <div className='order-summary-total'>
+              <p>Total: </p><h3>375</h3>
+            </div>
+            <button className='proceed-to-pay'>Proceed to pay</button>
+          </form>
+        </div>
     </div>
   )
 }
