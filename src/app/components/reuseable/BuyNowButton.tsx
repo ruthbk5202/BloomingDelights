dbdeaf12c773b00f73253dc94b465ac67344050a
Ruth Bishwakarma <ruthbishwokarma194@gmail.com>
2025-07-16 23:45:33 +0545

    fix: fixed the unuse code

--- src/app/components/reuseable/BuyNowButton.tsx
+++ src/app/components/reuseable/BuyNowButton.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { useRouter } from 'next/navigation';
-import {useState} from 'react';
+// import React from 'react';
+// import { useRouter } from 'next/navigation';
+// import {useState} from 'react';
 
-const BuyNowButton = () => {
-  const [_buyNow, setBuyNow] = useState(false);
-  const router = useRouter(
+// const BuyNowButton = () => {
+//   const [_buyNow, setBuyNow] = useState(false);
+//   const router = useRouter(
     
-  );
-  const handleBuyNow = () => {
-    setBuyNow(true);
+//   );
+//   const handleBuyNow = () => {
+//     setBuyNow(true);
    
-    router.push('/buynow');
-  };
-  return (
-    <div>
-        <button className="px-4 py-2 rounded bg-pink-500 text-white custom-btn" onClick={handleBuyNow}>
-            Buy Now
-        </button>
-    </div>
-  )
-}
+//     router.push('/buynow');
+//   };
+//   return (
+//     <div>
+//         <button className="px-4 py-2 rounded bg-pink-500 text-white custom-btn" onClick={handleBuyNow}>
+//             Buy Now
+//         </button>
+//     </div>
+//   )
+// }
 
-export default BuyNowButton;
+// export default BuyNowButton;
