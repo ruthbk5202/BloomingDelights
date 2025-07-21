364b15b084f6f4ff87cc740a7b5e0a2c0abfac91
Ruth Bishwakarma <ruthbishwokarma194@gmail.com>
2025-07-21 22:56:25 +0545

    feat: add description add-to-cart page layout

--- src/app/descriptionaddtocart/page.tsx
+++ src/app/descriptionaddtocart/page.tsx
@@ -1,42 +1,97 @@
-import React from 'react';
+import React from "react";
 import { RiDeleteBin7Line } from "react-icons/ri";
 import { CiShop } from "react-icons/ci";
 import { PiGreaterThan } from "react-icons/pi";
+import { CiLocationOn } from "react-icons/ci";
 import Image from "next/image";
 import "./description.css";
 
 const DescriptionAfterAddtocart = () => {
   return (
-    <div className='container-description'>
-        <div className='description-addtocart-container'>
-            <div className='select-all'>
-                <div className='select-checkbox'>
-                <input type="checkbox"className='check-box-for-selection'></input>
-                <p>Select ALL </p></div>
-                <div className='delete-button'>
-                    <RiDeleteBin7Line />
-                <p>Delete</p>
+    <div className="container-description">
+      <div className="description-addtocart-container">
+        {/* Left Column - Products Section */}
+        <div className="left-column">
+          <div className="select-all">
+            <div className="select-checkbox">
+              <input type="checkbox" className="check-box-for-selection" />
+              <p>Select ALL</p>
+            </div>
+            <div className="delete-button">
+              <RiDeleteBin7Line />
+              <p>Delete</p>
+            </div>
+          </div>
 
-                </div>
+          <div className="product-list">
+            <div className="product-list-nav">
+              <input type="checkbox" />
+              <CiShop />
+              <p>Ecom store</p>
+              <PiGreaterThan />
             </div>
-            <div className='order-summary'>
+            <div className="product-list-items">
+              <input type="checkbox" />
+              <Image
+                src="/assets/images/flower-1.jpg"
+                alt="Product"
+                className="description-image"
+                width={80}
+                height={80}
+              />
+              <p className="product-description">description</p>
+              <div className="price-container">
+                <p className="current-price">Rs. 40,000</p>
+                <p className="original-price">Rs. 41,500</p>
+              </div>
+              <div className="quantity-control">
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </div>
             </div>
-            <div className='product-list'>
-                <div className='product-list-nav'>
-                    <input type="checkbox"></input>
-                    <CiShop />
-                    <p> Ecom store</p>
-                      <PiGreaterThan />
-                </div>
-                <div>
-                    <input type="checkbox" ></input>
-                    <Image src="" alt=""></Image>
-                    <p></p>
-                </div>
-</div>
+          </div>
         </div>
+
+        {/* Right Column - Order Summary */}
+        <div className="order-summary">
+          <div className="order-summary-header">
+            <p>Location</p>
+            <div className="add-shipping">
+              <CiLocationOn />
+              <p>Add Shipping Address</p>
+            </div>
+          </div>
+          <div className="summary-content">
+            <h3>Order Summary</h3>
+            <div className="order-summary-details">
+              <p>Subtotal(0 items)</p>
+              <p>Rs. 0</p>
+            </div>
+            <div className="shipping-fee-details">
+              <p>Shipping Fee</p>
+              <p>Rs. 0</p>
+            </div>
+            <div className="apply-coupon">
+              <input 
+                type="text" 
+                placeholder="Enter coupon code" 
+                className="apply-search-box" 
+              />
+              <button className="button-apply">Apply</button>
+            </div>
+            <div className="order-total">
+              <p>Total</p>
+              <p>Rs. 0</p>
+            </div>
+            <button className="proceed-to-checkout-button">
+              PROCEED TO CHECKOUT
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default DescriptionAfterAddtocart;
