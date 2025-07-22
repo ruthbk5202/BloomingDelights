423dd3abec64341da0aba1fdd14507bd04ad5285
Ruth Bishwakarma <ruthbishwokarma194@gmail.com>
2025-07-22 12:22:54 +0545

    feat: enhanced Delivery component with grid

--- src/app/components/deliverydate/Delivery.tsx
+++ src/app/components/deliverydate/Delivery.tsx
@@ -1,3 +1,5 @@
+'use client'; // Ensures this component renders only on client-side
+
 import React from 'react';
 import { FaCalendarAlt, FaTruck, FaUser, FaBriefcase } from 'react-icons/fa';
 import { IoStarSharp } from "react-icons/io5";
@@ -7,71 +9,46 @@ import "./delivery.css";
 
 const Delivery = () => {
   return (
-    <div>
+    <div className="delivery-container">
+      {/* Features Section */}
       <div className="features-container">
-        {/* Feature 1: Choose your own delivery date */}
-        <div className="feature-box">
-          <div className="feature-icon">
-            <FaCalendarAlt size={32} />
+        {[
+          { icon: <FaCalendarAlt size={32} />, text: "Choose your own delivery date" },
+          { icon: <FaTruck size={32} />, text: "Super fast delivery" },
+          { icon: <FaUser size={32} />, text: "Personal Contact" },
+          { icon: <FaBriefcase size={32} />, text: "Various business solutions" }
+        ].map((feature, index) => (
+          <div key={index} className="feature-box">
+            <div className="feature-icon">{feature.icon}</div>
+            <div className="feature-text">
+              <h3>{feature.text}</h3>
+            </div>
           </div>
-          <div className="feature-text">
-            <h3>Choose your own delivery date</h3>
-          </div>
-        </div>
-
-        {/* Feature 2: Super fast delivery */}
-        <div className="feature-box">
-          <div className="feature-icon">
-            <FaTruck size={32} />
-          </div>
-          <div className="feature-text">
-            <h3>Super fast delivery</h3>
-          </div>
-        </div>
-
-        {/* Feature 3: Personal contact */}
-        <div className="feature-box">
-          <div className="feature-icon">
-            <FaUser size={32} />
-          </div>
-          <div className="feature-text">
-            <h3>Personal Contact</h3>
-          </div>
-        </div>
-
-        {/* Feature 4: Various business solutions */}
-        <div className="feature-box">
-          <div className="feature-icon">
-            <FaBriefcase size={32} />
-          </div>
-          <div className="feature-text">
-            <h3>Various business </h3>
-            <h3>solutions</h3>
-          </div>
-        </div>
+        ))}
       </div>
 
+      {/* Rating Section */}
       <div className='excellent'>
-        <button className='button-review'>
-          <IoStarSharp />
-          <IoStarSharp />
-          <IoStarSharp />
-          <IoStarSharp />
-          <IoStarSharp /> 
+        <button className='button-review' aria-label="5-star rating">
+          {[...Array(5)].map((_, i) => (
+            <IoStarSharp key={i} />
+          ))}
         </button>
         <button className='button-excellent'>
           <h3>Excellent ! 7.8</h3>
           <p>From 2058 reviews</p>
         </button>
-        <button className='button-email'>
+        <button className='button-email' aria-label="Email us">
           <HiOutlineMailOpen />
         </button>
       </div>
 
-      <div className='why-bloomingdelights'>
+      {/* About Section */}
+      <section className='why-bloomingdelights'>
         <h1>Why BloomingDelights ?</h1>
         <h2 className='iamthinking'>Because I&apos;m thinking of you.</h2>
-        <div>
+        
+        <div className="about-content">
           <p>
             At Blooming Delights, You can easily order the most beautiful bouquets and floral gifts for any occasion. Whether it&apos;s your birthday, thankyou, declaration of love, or just a simple gesture, we&apos;ll make sure your flowers are delivered right toyour door with a personal message.
           </p>
@@ -85,35 +62,44 @@ const Delivery = () => {
           </ul>
         </div>
         
-        <Image 
-          src="/assets/images/why.jpg" 
-          className='why-img'
-          alt="Blooming Delights flowers"
-          width={500}
-          height={300}
-        />
+        <div className="why-image-wrapper">
+          <Image 
+            src="/assets/images/why.jpg" 
+            className='why-img'
+            alt="Blooming Delights flowers"
+            width={500}
+            height={300}
+            priority={false}
+            loading="lazy"
+          />
+        </div>
 
-        <p>
-          Our flowers are arranged daily with attention to detail and a passion for the craft. Thanks to our in-house delivery service, we can be sure your bouquet will be delivered with the utmost care and attention.
-          <br /><br />
-          Order before 1:00 PM? Same-day delivery is even possible (excluding Sundays and public holidays). Prefer a different delivery date? We&apos;re happy to arrange that. You choose the time, and we&apos;ll take care of the rest.
-          <br /><br />
-          Ordering for business? With a business account, you&apos;ll enjoy convenient features and exclusive benefits.
-          <br /><br />
-          *Please note: delivery to the himalayas is unfortunately no longer possible.
-        </p>
-      </div>
+        <div className="additional-info">
+          <p>
+            Our flowers are arranged daily with attention to detail and a passion for the craft. Thanks to our in-house delivery service, we can be sure your bouquet will be delivered with the utmost care and attention.
+            <br /><br />
+            Order before 1:00 PM? Same-day delivery is even possible (excluding Sundays and public holidays). Prefer a different delivery date? We&apos;re happy to arrange that. You choose the time, and we&apos;ll take care of the rest.
+            <br /><br />
+            Ordering for business? With a business account, you&apos;ll enjoy convenient features and exclusive benefits.
+            <br /><br />
+            *Please note: delivery to the himalayas is unfortunately no longer possible.
+          </p>
+        </div>
+      </section>
 
-      <div className='grid-blooming-delights'>
-        <div>
+      {/* Grid Section */}
+      <section className='grid-blooming-delights'>
+        <div className="grid-image">
           <Image 
             src="/assets/images/whom.jpg"
             alt="Surprise someone with flowers"
             width={500}
             height={300}
+            priority={false}
+            loading="lazy"
           />
         </div>
-        <div>
+        <div className="grid-content">
           <h3>Who will you surprise with a Bouquet?</h3>
           <p>
             With Blooming Delights you deliver a personal message with a beautiful bouquet. Professionally tied and delivered by a regional florist.
@@ -121,7 +107,7 @@ const Delivery = () => {
             And the best part is: it&apos;s super fast. With a few clicks you have not only arranged a bouquet, but also arranged a smile on the face of the recipient. A loving gesture that gives someone a moment of attention and lets them feel: hey I&apos;m thinking of you. This way you can be a little bit close even from a distance.
           </p>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
--- src/app/components/dialogcart/AddToCartSucceed.tsx
+++ src/app/components/dialogcart/AddToCartSucceed.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './AddToCartSucceed.css'; // You'll want to create this CSS file
 
 const AddToCartSucceed = () => {
   return (
-    <div>
-        <div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check-circle">
-                <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-                <polyline points="22 4 12 14.01 9 11.01"></polyline>
-            </svg>
-        </div>
+    <div className="success-container">
+      <div className="success-icon">
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          width="48" 
+          height="48" 
+          viewBox="0 0 24 24" 
+          fill="none" 
+          stroke="#4BB543" 
+          strokeWidth="3" 
+          strokeLinecap="round" 
+          strokeLinejoin="round" 
+          className="feather feather-check-circle"
+        >
+          <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+          <polyline points="22 4 12 14.01 9 11.01"></polyline>
+        </svg>
+      </div>
+      <h2 className="success-message">Item added to cart successfully!</h2>
     </div>
-  )
-}
+  );
+};
 
 export default AddToCartSucceed;
