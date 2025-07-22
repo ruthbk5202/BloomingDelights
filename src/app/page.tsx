1a10841b2963d9029abe31e5cfbe61d117860d8c
Ruth Bishwakarma <ruthbishwokarma194@gmail.com>
2025-07-22 14:41:30 +0545

    refactor: move routing setup out of home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
-import { BrowserRouter } from "react-router-dom";
+
 import HomePage from "./HomePage/page";
 
 export default function Home() {
   return (
     <div>
-      <BrowserRouter>
-        <HomePage />
-      </BrowserRouter>
+     <HomePage/>
     </div>
   );
 }
